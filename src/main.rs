b271e6aa0d01c617b3fc236287331ea81593b758
hnamhocit <hnamhocit@gmail.com>
2025-03-19 21:17:15 +0700

    simple rust

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    // Simple HTTP server example
+    use std::io::{Read, Write};
+    use std::net::{TcpListener, TcpStream};
+    use std::thread;
+
+    fn handle_client(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+
+        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello from Rust Server!</h1></body></html>\r\n";
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
+    fn start_server() {
+        let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+        println!("Server started at http://127.0.0.1:8080");
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    thread::spawn(|| {
+                        handle_client(stream);
+                    });
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+            }
+        }
+    }
+
+    // Uncomment the next line to start the server
+    start_server();
+}
